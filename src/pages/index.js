ef1a95649877adf966bc2d44405bac3c5ccae386
naila <nailaparwin@gmail.com>
2020-12-22 14:29:16 +0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div>
+        Hello world!
+      </div>
+      <button 
+      class="snipcart-add-item"
+      data-item-id="formal-shoe"
+      data-item-price="25"
+      data-item-description="good for change"
+      data-item-url="/"
+      data-item-image="https://www.net-a-porter.com/en-ae/shop/product/valentino/shoes/high-heel/17957409490182112"
+      data-item-name="formal-shoe"
+      data-item-custom1-name="Size"
+      data-item-custom2-options="8|9[+5]|10[+10]"
+      data-item-custom2-name="Info"
+      data-item-custom2-type="checkbox"
+      > Check Out</button>
+  </div>
+  )
 }
